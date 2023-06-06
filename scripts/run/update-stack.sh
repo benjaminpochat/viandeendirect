a5b7952347289a515bbd17396716c7a79c05efe2
Benjamin POCHAT <benjamin.pochat@gmail.com>
2023-06-07 00:11:14 +0200

    première stack docker-compose complète

--- scripts/run/update-stack.sh
+++ scripts/run/update-stack.sh
@@ -1,2 +1,4 @@
-docker compose -f docker-compose.yml pull
-docker compose -f docker-compose.yml up viandeendirect
+docker compose -f compose.yml pull
+#docker stack deploy -c compose.yml viandeendirect
+#docker compose -f compose.yml up --detach
+docker compose -f compose.yml up
