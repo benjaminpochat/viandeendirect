fd782dc10d5afd1ed8ffe2a0ef600fa3b6af323d
Benjamin POCHAT <benjamin.pochat@gmail.com>
2023-11-30 21:09:46 +0000

    styles

---
+++ frontend/app/src/domains/production/components/ProductionCard.tsx
@@ -0,0 +1,20 @@
+import BeefProductionCard from "./BeefProductionCard.tsx"
+
+export default function ProductionCard({
+    production: production, 
+    showActions: showActions,
+    setPackageModificationLayoutContent: setPackageModificationLayoutContent
+}) {
+
+    switch (production.productionType) {
+        case 'BeefProduction':
+            return <BeefProductionCard 
+                        key={production.id}
+                        production={production} 
+                        showActions={showActions}
+                        setPackageModificationLayoutContent={setPackageModificationLayoutContent}>
+                    </BeefProductionCard>
+        default :
+            return <></>
+    }
+}
