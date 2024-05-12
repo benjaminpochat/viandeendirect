1697ff3b929afb81cdea5098ee7c25d634a799c4
Benjamin POCHAT <benjamin.pochat@gmail.com>
2024-05-12 09:34:04 +0200

    fix cors problems with spring security 6

--- backend/app/src/main/java/eu/viandeendirect/security/configuration/SecurityConfiguration.java
+++ backend/app/src/main/java/eu/viandeendirect/security/configuration/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
@@ -29,12 +30,8 @@ class SecurityConfiguration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfiguration.class);
 
-    @Value("${PRODUCER_FRONTEND_URL:http://localhost:3000}")
-    String producerFrontendUrl;
-
-    @Value("${CUSTOMER_FRONTEND_URL:http://localhost:3000}")
-    String customerFrontendUrl;
-
+    public static final String ROLE_PRODUCER = "PRODUCER";
+    public static final String ROLE_CUSTOMER = "CUSTOMER";
 
     private final KeycloakLogoutHandler keycloakLogoutHandler;
 
@@ -48,18 +45,6 @@ class SecurityConfiguration {
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        LOGGER.debug("start SecurityConfiguration#corsConfigurationSource");
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(customerFrontendUrl, producerFrontendUrl));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        LOGGER.debug("stop SecurityConfiguration#corsConfigurationSource");
-        return source;
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         LOGGER.debug("start SecurityConfiguration#filterChain");
         http.authorizeHttpRequests(authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer
@@ -67,20 +52,23 @@ class SecurityConfiguration {
                         .requestMatchers("/swagger-ui").permitAll()
                         .requestMatchers("/addresses", "/addresses/**").permitAll()
                         .requestMatchers("/sales", "/sales/**").permitAll()
-                        .requestMatchers("/packageTemplates", "/packageTemplates/**").hasRole("PRODUCER")
+                        .requestMatchers("/packageTemplates", "/packageTemplates/**").hasRole(ROLE_PRODUCER)
                         .requestMatchers("/productions", "/productions/**").permitAll()
                         .requestMatchers("/beefProductions", "/beefProductions/**").permitAll()
                         .requestMatchers("/honneyProductions", "/honneyProductions/**").permitAll()
-                        .requestMatchers("/customers", "/customers/**").authenticated()
-                        .requestMatchers("/producers", "/producers/**").hasRole("PRODUCER")
-                        .requestMatchers("/orders", "/orders/**").hasRole("PRODUCER")
+                        .requestMatchers("/customers", "/customers/**").hasAnyRole(ROLE_PRODUCER, ROLE_CUSTOMER)
+                        .requestMatchers("/producers", "/producers/**").hasRole(ROLE_PRODUCER)
+                        .requestMatchers("/orders", "/orders/**").hasRole(ROLE_PRODUCER)
                         .requestMatchers("/error").permitAll()
+                        //TODO : enable role protection
+                        //.requestMatchers("/payments/stripe/**").hasRole(ROLE_PRODUCER)
                         .requestMatchers("/payments/stripe/**").permitAll()
                 );
         http.oauth2Login(Customizer.withDefaults());
         http.logout(logoutCustomizer -> logoutCustomizer.addLogoutHandler(keycloakLogoutHandler).logoutSuccessUrl("/"));
         http.oauth2ResourceServer(oAuth2ResourceServerConfigurer -> oAuth2ResourceServerConfigurer.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter())));
         http.cors(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
         LOGGER.debug("stop SecurityConfiguration#filterChain");
         return http.build();
     }
